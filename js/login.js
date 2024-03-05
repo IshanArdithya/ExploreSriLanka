e3ac722b9056c97b763976f0f66546c305f81761
Chavindu Jayakody <chavindujayakody2001@gmail.com>
2024-03-06 03:03:58 +0530

    Login UI Updated

--- js/login.js
+++ js/login.js
@@ -9,3 +9,34 @@ sign_up_btn.addEventListener("click", () => {
 sign_in_btn.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
 });
+
+const nextBtn = document.querySelector('.next-btn');
+const backBtn = document.querySelector('.back-btn-form');
+const signupBtn = document.querySelector('.signup-btn');
+const firstFields = document.querySelectorAll('.sign-up-form .input-field:nth-of-type(-n+3)');
+const hiddenFields = document.querySelectorAll('.sign-up-form .input-field:nth-of-type(n+4)');
+const buttonContainer = document.querySelector('.button-container');
+
+nextBtn.addEventListener('click', () => {
+  firstFields.forEach(field => {
+    field.style.display = 'none';
+  });
+  hiddenFields.forEach(field => {
+    field.style.display = 'grid';
+  });
+  nextBtn.style.display = 'none';
+  backBtn.style.display = 'block';
+  signupBtn.style.display = 'block';
+});
+
+backBtn.addEventListener('click', () => {
+  firstFields.forEach(field => {
+    field.style.display = 'grid';
+  });
+  hiddenFields.forEach(field => {
+    field.style.display = 'none';
+  });
+  nextBtn.style.display = 'block';
+  backBtn.style.display = 'none';
+  signupBtn.style.display = 'none';
+});
