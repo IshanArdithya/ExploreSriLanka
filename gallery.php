a0c60916dddac8a1ec421873f12d3219280fce76
MMNLakna <136817118+MMNLakna@users.noreply.github.com>
2024-04-05 00:55:14 +0530

    gallery,package updated. user profile added.

---
+++ gallery.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/responsive.css">
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css">
+  <title>Gallery- Explore Srilanka</title>
+</head>
+
+<body>
+
+  <!-- Header -->
+  <?php
+  include 'components/header.php';
+  ?>
+
+  <div class="top-image">
+    <h1 class="headings sub-heading"></h1>
+  </div>
+
+  <!-- Breadcrumbs -->
+  <div class="container">
+    <div class="row">
+      <ol class="breadcrumb">
+        <li><a href="../index.php" title="Explore Sri Lanka" class="bolds">Home</a></li>
+        <li class="active">Gallery</li>
+      </ol>
+    </div>
+
+    <div class="container">
+      <h1 class="headings mini-heading">Gallery</h1>
+      <div class="gallery-main-container">
+
+        <div class="gallery-image-container">
+          <img src="./gallery/beach1.jpg" alt="">
+        </div>
+        <div class="gallery-image-container">
+          <img src="./gallery/beach2.jpg" alt="">
+        </div>
+
+        <div class="gallery-image-container tall">
+          <img src="./gallery/water9.jpg" alt="">
+        </div>
+        <div class="gallery-image-container">
+          <img src="./gallery/beach4.jpg" alt="">
+        </div>
+        <div class="gallery-image-container">
+          <img src="./gallery/beach5.jpeg" alt="">
+        </div>
+        <div class="gallery-image-container tall">
+          <img src="./gallery/beach6.jpg" alt="">
+        </div>
+        <div class="gallery-image-container">
+          <img src="./gallery/beach7.jpg" alt="">
+        </div>
+        <div class="gallery-image-container">
+          <img src="./gallery/beach8.jpg" alt="">
+        </div>
+        <div class="gallery-image-container">
+          <img src="./gallery/beach9.jpeg" alt="">
+        </div>
+
+        <div class="gallery-image-container">
+          <img src="./gallery/wild1.jpg" alt="">
+        </div>
+        <div class="gallery-image-container tall">
+          <img src="./gallery/wild10.jpg" alt="">
+        </div>
+
+        <div class="gallery-image-container tall">
+          <img src="./gallery/wild12.png" alt="">
+        </div>
+        <div class="gallery-image-container">
+          <img src="./gallery/wild3.jpeg" alt="">
+        </div>
+
+        <div class="gallery-image-container">
+          <img src="./gallery/wild4.jpeg" alt="">
+        </div>
+        <div class="gallery-image-container tall">
+          <img src="./gallery/wild5.jpg" alt="">
+        </div>
+        <div class="gallery-image-container tall">
+          <img src="./gallery/wild6.jpg" alt="">
+        </div>
+        <div class="grid-item">
+          <img src="./gallery/wild9.jpg" alt="">
+        </div>
+
+        <div class="gallery-image-container tall">
+          <img src="./gallery/wild11.jpg" alt="">
+        </div>
+
+        <div class="gallery-image-container tall">
+          <img src="./gallery/culture1.jpg" alt="">
+        </div>
+        <div class="gallery-image-container tall">
+          <img src="./gallery/culture2.jpg" alt="">
+        </div>
+        <div class="gallery-image-container">
+          <img src="./gallery/culture3.jpeg" alt="">
+        </div>
+
+        <div class="gallery-image-container tall">
+          <img src="./gallery/heritage6.jpg" alt="">
+        </div>
+        <div class="gallery-image-container">
+          <img src="./gallery/culture5.jpeg" alt="">
+        </div>
+        <div class="gallery-image-container">
+          <img src="./gallery/culture6.webp" alt="">
+        </div>
+        <div class="gallery-image-container">
+          <img src="./gallery/culture7.jpg" alt="">
+        </div>
+        <div class="gallery-image-container">
+          <img src="./gallery/culture8.jpg" alt="">
+        </div>
+
+        <div class="gallery-image-container tall">
+          <img src="./gallery/water1.jpg" alt="">
+        </div>
+        <div class="gallery-image-container">
+          <img src="./gallery/water2.jpg" alt="">
+        </div>
+        <div class="gallery-image-container">
+          <img src="./gallery/water3.jpg" alt="">
+        </div>
+        <div class="gallery-image-container tall">
+          <img src="./gallery/water4.jpg" alt="">
+        </div>
+        <div class="gallery-image-container">
+          <img src="./gallery/water5.jpg" alt="">
+        </div>
+        <div class="gallery-image-container">
+          <img src="./gallery/water6.jpg" alt="">
+        </div>
+        <div class="gallery-image-container">
+          <img src="./gallery/water7.jpeg" alt="">
+        </div>
+        <div class="gallery-image-container">
+          <img src="./gallery/water8.jpg" alt="">
+        </div>
+        <div class="gallery-image-container tall">
+          <img src="./gallery/water9.jpg" alt="">
+        </div>
+
+        <div class="gallery-image-container tall">
+          <img src="./gallery/adventure1.jpg" alt="">
+
+        </div>
+        <div class="gallery-image-container">
+          <img src="./gallery/adventure2.jpg" alt="">
+        </div>
+        <div class="gallery-image-container">
+          <img src="./gallery/adventure3.jpg" alt="">
+        </div>
+        <div class="gallery-image-container tall">
+          <img src="./gallery/adventure4.jpg" alt="">
+        </div>
+        <div class="gallery-image-container tall">
+          <img src="./gallery/adventure5.jpg" alt="">
+        </div>
+        <div class="grid-item">
+          <img src="./gallery/adventure6.jpg" alt="">
+        </div>
+        <div class="gallery-image-container tall">
+          <img src="./gallery/adventure7.jpg" alt="">
+        </div>
+
+        <div class="gallery-image-container tall">
+          <img src="./gallery/adventure8.jpg" alt="">
+        </div>
+        <div class="gallery-image-container">
+          <img src="./gallery/adventure9.jpeg" alt="">
+        </div>
+        <div class="gallery-image-container tall">
+          <img src="./gallery/adventure10.jpg" alt="">
+        </div>
+
+        <div class="gallery-image-container">
+          <img src="./gallery/culture3.jpeg" alt="">
+        </div>
+
+        <div class="gallery-image-container tall">
+          <img src="./gallery/heritage7.jpg" alt="">
+        </div>
+        <div class="gallery-image-container">
+          <img src="./gallery/culture5.jpeg" alt="">
+        </div>
+        <div class="gallery-image-container">
+          <img src="./gallery/culture6.webp" alt="">
+        </div>
+        <div class="gallery-image-container">
+          <img src="./gallery/culture7.jpg" alt="">
+        </div>
+
+      </div>
+    </div>
+
+  </div>
+
+
+  <!-- Footer -->
+  <?php
+  include 'components/footer.php';
+  ?>
+
+  <script src="js/script.js"></script>
+
+</body>
+
+</html>
---
+++ user.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/responsive.css">
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.4.2/css/fontawesome.min.css">
+  <title>Product-Details</title>
+
+</head>
+
+<body>
+
+  <!-- Header -->
+  <?php
+  include 'components/header.php';
+  ?>
+
+</head>
+<body >
+  <div class="container">
+    <div class="profile-settings-container">
+      <h1>Account settings</h1>
+      <div class="profile-sub-container">
+        <div class="profile-image">
+          <img src="./Images/ella1.jpg" alt="Profile Picture">
+          <button class="upload-profile-btn">Upload new photo</button>
+         
+          <button class="reset-password-btn upload-profile-btn">Reset Password</button>
+        </div>
+        <form class="profile-settings-details">
+        <div class="name-inputs">
+          <div class="profile-group">
+            
+
+            <label class="profile-label">First Name</label>
+            <div>
+            <input type="text" class="profile-control control-names profile-settings-row" value="nmaxwell">
+            </div>
+          </div>
+          <div class="profile-group">
+            <label class="profile-label">Last Name</label>
+            <div>
+            <input type="text" class="profile-control control-names profile-settings-row" value="nmaxwell">
+            </div>
+          </div>
+
+          </div>
+          <div class="profile-group">
+            <label class="profile-label">E-mail</label>
+            <div>
+            <input type="text" class="profile-control profile-settings-row" value="nmaxwell@mail.com"  disabled>
+            </div>
+          </div>
+          <div class="profile-group">
+            <label class="profile-label">Phone</label>
+            <div>
+            <input type="text" class="profile-control" value="+0 (123) 456 7891">
+            </div>
+          </div>
+        
+          <div class="profile-group">
+            <label class="profile-label">Birthday</label>
+            <div>
+            <input type="date" class="profile-control" value="1995-05-03"  disabled>
+            </div>
+          </div>
+          <div class="profile-group">
+            <label class="profile-label">Country</label>
+            <div>
+            <input type="text" class="profile-control" value="Canada"  disabled>
+            </div>
+          </div>
+          <div class="profile-setting-buttons">
+            <button class="save-profile-btn">Save changes</button>
+            <button class="cancel-profile-btn">Cancel</button>
+          </div>
+          <!-- Password section -->
+          <div class="password-section">
+            <div class="profile-group">
+              <label class="profile-label">Current Password</label>
+              <div>
+              <input type="password" class="profile-control">
+            </div>
+            <div class="profile-group">
+              <label class="profile-label">New Password</label>
+              <div>
+              <input type="password" class="profile-control">
+              </div>
+            </div>
+            <div class="profile-group">
+              <label class="profile-label">Confirm Password</label>
+              <div>
+              <input type="password" class="profile-control">
+              </div>
+            </div>
+            <div class="profile-setting-buttons">
+            <button class="save-profile-btn">Reset Password</button>
+            <button class="cancel-profile-btn">Cancel</button>
+          </div>
+          </div>
+         
+        </form>
+      </div>
+
+    </div>
+      </div>
+  </div>
+
+
+
+  <script src="js/script.js"></script>
+  <script>
+    document.addEventListener('DOMContentLoaded', function () {
+      // Toggle password section visibility
+      const resetPasswordBtn = document.querySelector('.reset-password-btn');
+      const passwordSection = document.querySelector('.password-section');
+
+      resetPasswordBtn.addEventListener('click', function () {
+        passwordSection.style.display = passwordSection.style.display === 'none' ? 'block' : 'none';
+      });
+    });
+  </script>
+
+<?php include 'components/footer.php'; ?>
+</body>
+
+</html>
