ccbf81098ac56d5eeafc0de9e7c83a89140ad7ad
MMNLakna <136817118+MMNLakna@users.noreply.github.com>
2024-04-26 03:36:45 +0530

    about.php changes added

--- index.php
+++ index.php
@@ -535,10 +535,10 @@
                             </div>
                         </div>
                         <div class="carousel swiper-slide">
-                            <img src="Images/slide5.jpg" alt="">
+                            <img src="Images/slide6.webp" alt="">
                             <div class="carousel-img-overlay">
                                 <div class="img-content">
-                                    <h2>Lovers Leap</h2>
+                                    <h2>Pigeon Island</h2>
                                     <div class="hidden-content-carousel">
                                         <span><i class="fa fa-clock"></i>3 Days</span>
                                         <span><i class="fa fa-user"></i>6 Persons</span>
