0e1a8162d48e0c5cb28294407ed8e3e0496b310a
MMNLakna <136817118+MMNLakna@users.noreply.github.com>
2024-04-29 21:07:14 +0530

    Update Wildlife-Safari-Adventure.php

--- tours/Wildlife-Safari-Adventure.php
+++ tours/Wildlife-Safari-Adventure.php
@@ -475,6 +475,9 @@ function handleAddReservation()
   include '../components/footer.php';
   ?>
 
+<script src="../node_modules/jquery/dist/jquery.js"></script>
+  <script src="../node_modules/owl.carousel/dist/owl.carousel.min.js"></script>
+
   <script>
     $(document).ready(function() {
       $('#owl1').owlCarousel({
