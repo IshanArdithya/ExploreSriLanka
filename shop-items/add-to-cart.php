7a0a2bd2449f7a91fbadfffe2b65ce521dc74387
Ishan Ardithya Bandarigoda <55695411+IshanArdithya@users.noreply.github.com>
2024-07-22 11:30:58 +0530

    Update add-to-cart.php

--- shop-items/add-to-cart.php
+++ shop-items/add-to-cart.php
@@ -1,28 +1,22 @@
 <?php
 session_start();
 
-// Check if item ID and quantity are provided
 if (isset($_POST['item_id'], $_POST['quantity'])) {
     $itemId = $_POST['item_id'];
     $quantity = $_POST['quantity'];
 
-    // Check if the item is already in the cart
     if (!isset($_SESSION['cart'][$itemId])) {
-        // If not, add the item to the cart with the specified quantity
         $_SESSION['cart'][$itemId] = $quantity;
     } else {
-        // If the item is already in the cart, increment its quantity by the specified amount
         $_SESSION['cart'][$itemId] += $quantity;
     }
 
-    // Send a JSON response indicating success and the updated item count
     $response = [
         'success' => true,
         'itemCount' => count($_SESSION['cart'])
     ];
     echo json_encode($response);
 } else {
-    // If item ID or quantity is not provided, send an error response
     $response = [
         'success' => false,
         'message' => 'Item ID or quantity not provided'
